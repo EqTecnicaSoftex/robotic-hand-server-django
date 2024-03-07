8a024a92397e971b9c95a13e7a103b649144653c
Limaivson <ils3@cin.ufpe.br>
2024-03-07 16:18:56 -0300

    refactor-message-return

--- controlServer/controlServer/views.py
+++ controlServer/controlServer/views.py
@@ -11,36 +11,33 @@ def send_control_denso(request):
         denso.receive_positions_server()
         denso.clear_receive_positions()
         print('Received control_denso: ' + control_denso)
-        return JsonResponse({'message': 'Received control_denso: ' + control_denso})
+        return JsonResponse({'message': 'Posições ' + control_denso + ' recebidas com sucesso'})
     else:
-        return JsonResponse({'message': 'Invalid request method'}, status=400)
+        return JsonResponse({'message': 'Método de solicitação inválido'}, status=400)
 
 
 def send_control_hand(request):
     if request.method == 'POST':
         control_hand = request.POST.get('move_hand')
         print(control_hand)
-        return JsonResponse({'message': 'Received control_hand: ' + control_hand})
+        return JsonResponse({'message': 'Abertura: ' + control_hand + ' da mão recebida com sucesso'})
     else:
-        return JsonResponse({'message': 'Invalid request method'}, status=400)
+        return JsonResponse({'message': 'Método de solicitação inválido'}, status=400)
 
 
 def move_denso(request):
     if request.method == 'POST':
-        print(denso.positions)
         if denso.positions == '':
-            return JsonResponse({'message': 'No positions received'}, status=400)
+            return JsonResponse({'message': 'Posições não recebidas'}, status=400)
         else:
-            print('Moving DENSO to positions: ' + denso.positions)
-            return JsonResponse({'message': 'Received positions: ' + denso.positions})
+            return JsonResponse({'message': 'Movendo o denso para a posição: ' + denso.positions})
     else:
-        return JsonResponse({'message': 'Invalid request method'}, status=400)
+        return JsonResponse({'message': 'Método de solicitação inválido'}, status=400)
 
 
 def finalize_denso(request):
     if request.method == 'POST':
         final_position = '0,0,0,0,0,0'
-        print(f'Final position Denso: {final_position}')
-        return JsonResponse({'message': 'Finalizing DENSO'})
+        return JsonResponse({'message': 'Movendo o denso para a posição: ' + final_position})
     else:
-        return JsonResponse({'message': 'Invalid request method'}, status=400)
+        return JsonResponse({'message': 'Método de solicitação inválido'}, status=400)
